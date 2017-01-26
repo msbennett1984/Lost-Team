c1e01e76a26ff59e0d1c897ea53723c922daef14
Sherry Bennett <msbennett84@gmail.com>
2017-01-26 18:02:02 -0500

    Add instance of Player to main function.

--- src/lostteam/LostTeam.java
+++ src/lostteam/LostTeam.java
@@ -5,6 +5,8 @@
  */
 package lostteam;
 
+import byui.cit260.lostteam.model.Player;
+
 /**
  *
  * @author Sherry Bennett <msbennett84@gmail.com>
@@ -15,7 +17,7 @@ public class LostTeam {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Player playerOne = new Player();
     }
     
 }
