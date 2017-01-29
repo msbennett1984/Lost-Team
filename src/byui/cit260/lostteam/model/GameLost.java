e0a409dfd9089e6cec1676950a614b5767095f22
nanaachina <adm-achina@BFSGL-IT.bluefs.com.gh>
2017-01-29 23:18:22 +0000

    Initial Commit

---
+++ src/byui/cit260/lostteam/model/GameLost.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.lostteam.model;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author adm-achina
+ */
+public class GameLost implements Serializable{
+
+    // class instance variable
+    private String totalTime;
+    private double LastPlayer;
+    private Player player;
+    private static GameLost instance;
+
+    public GameLost() {
+    }
+
+    
+    
+    public String getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(String totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public double getLastPlayer() {
+        return LastPlayer;
+    }
+
+    public void setLastPlayer(double LastPlayer) {
+        this.LastPlayer = LastPlayer;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public static GameLost getInstance() {
+        return instance;
+    }
+
+    public static void setInstance(GameLost instance) {
+        GameLost.instance = instance;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + Objects.hashCode(this.totalTime);
+        hash = 59 * hash + (int) (Double.doubleToLongBits(this.LastPlayer) ^ (Double.doubleToLongBits(this.LastPlayer) >>> 32));
+        hash = 59 * hash + Objects.hashCode(this.player);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final GameLost other = (GameLost) obj;
+        if (!Objects.equals(this.totalTime, other.totalTime)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.LastPlayer) != Double.doubleToLongBits(other.LastPlayer)) {
+            return false;
+        }
+        if (!Objects.equals(this.player, other.player)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "GameLost{" + "totalTime=" + totalTime + ", LastPlayer=" + LastPlayer + ", player=" + player + '}';
+    }
+    
+    
+}
---
+++ src/byui/cit260/lostteam/model/Scene.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.lostteam.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author adm-achina
+ */
+public class Scene implements Serializable {
+    
+    
+    //class instance variables
+    private String description;
+    private String signSymbol;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSignSymbol() {
+        return signSymbol;
+    }
+
+    public void setSignSymbol(String signSymbol) {
+        this.signSymbol = signSymbol;
+    }
+    
+    
+    //hasCode Func.
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + Objects.hashCode(this.description);
+        hash = 89 * hash + Objects.hashCode(this.signSymbol);
+        return hash;
+    }
+
+        
+    @Override
+    public String toString() {
+        return "Scene{" + "description=" + description + ", signSymbol=" + signSymbol + '}';
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Scene other = (Scene) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.signSymbol, other.signSymbol)) {
+            return false;
+        }
+        return true;
+    }
+
+        
+    
+    
+    
+}
