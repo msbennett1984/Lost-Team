03c4acd479650d3135b3c13aeebae17149acaece
Sherry Bennett <msbennett84@gmail.com>
2017-02-16 19:22:57 -0500

    Week 7 Team Assignment: Instructions page 1-21

---
+++ src/byui/cit260/lostteam/view/StartProgramView.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.lostteam.view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sherry Bennett <msbennett84@gmail.com>
+ */
+public class StartProgramView {
+    
+    private String promptMessage;
+    
+    
+    public StartProgramView() {
+        
+        // promptMessage = "Please enter your name"
+        this.promptMessage = "\nPlease enter your name: ";
+        
+        //display the banner when view is created
+        this.displayBanner();
+        
+    }
+
+    private void displayBanner() {
+        
+        System.out.println(
+              "\n************************************************"
+            + "\n                                               *"
+            + "\n* In this game, you will be taking on the role *"
+            + "\n* of a person who has woken up with            *"
+            + "\n* dissociative amenesia. You wake up in a      *"
+            + "\n* laboratory subway car with no memory of who  *"
+            + "\n* you are and how you got there. Your nose is  *"
+            + "\n* broken and your wallet is missing. You have  *"
+            + "\n* no coat, and despite it being winder, you    *"
+            + "\n* seem to be sun burnt. After a brief,         *"
+            + "\n* exploration, you discover that you have been *"
+            + "\n* poisoned. You have a limited amount of time  *"
+            + "\n* to solve your mystery and take the antidote  *"
+            + "\n* before it is too late.                       *"
+            + "\n*                                              *"
+            + "\n* Everything that you need to solve the mystery*"
+            + "\n* is found in your city within the locations   *"
+            + "\n* on your map.                                 *"
+            + "\n*                                              *"
+            + "\n* The game begins as you wake up in the subway *"
+            + "\n* car. During the game, you will need to       *"
+            + "\n* explore the locations on your map, find and  *"
+            + "\n* collect physical clues, and solve the        *"
+            + "\n* mystery. You have successfully complete the  *"
+            + "\n* game after you have recovered your memory    *"
+            + "\n* and found the antidote.                      *"
+            + "\n*                                              *"
+            + "\n* Gook luck and have fun in this adventure!    *"
+            + "\n                                               *"
+            + "\n************************************************"
+            );
+    }
+    
+    /**
+     * displays the start program view
+     */
+    public void displayStartProgramView() {
+        /*
+        do
+            Prompt for and get the input value
+            if (value == “Q”) then
+                return
+
+            do requested action and display the next view
+        while the view is not done
+        */
+        
+        boolean done = false; // set flag to not done
+        do {
+            // prompt for and get players name
+            String playersName = this.getPlayersName();
+            if (playersName.toUpperCase().equals("Q")) //user wants to quit
+                return; // exit the game
+            
+            // do the requested action and display the next view
+            done = this.doAction(playersName);
+            
+        } while (!done);
+    }
+
+    private String getPlayersName() {
+        /*
+         WHILE a valid value has not been entered
+            DISPLAY a message prompting the user to enter a value
+            GET the value entered from keyboard
+            Trim front and trailing blanks off of the value
+ 
+            IF the length of the value is blank THEN
+                DISPLAY “Invalid value: The value cannot be blank”
+                CONTINUE
+            ENDIF
+ 
+            BREAK
+ 
+            ENDWHILE
+            RETURN value 
+        */
+    
+        Scanner keyboard = new Scanner(System.in); //get infile for keyboard
+        String value = ""; // value to be returned
+        boolean valid = false; // initialize to not valid
+        
+        while (!valid) { // loop while an invalid value is enter
+            System.out.println("\n" + this.promptMessage);
+            
+            value = keyboard.nextLine(); // get next line typed on keyboard
+            value = value.trim(); // trim off leading and trailing blanks
+            
+            if (value.length() < 1) { // value is blank
+                System.out.println("\nInvalid value: value can not be blank");
+                continue;     
+        }
+            
+        break; // end the loop    
+        }
+    return value; // return the value entered
+    }
+
+    private boolean doAction(String playersName) {
+        System.out.println("\n*** doAction() called");
+        return true;
+    }
+    
+}
--- src/lostteam/LostTeam.java
+++ src/lostteam/LostTeam.java
@@ -17,6 +17,7 @@ import byui.cit260.lostteam.model.GameLost;
 import byui.cit260.lostteam.model.Scene;
 
 import byui.cit260.lostteam.model.Player;
+import byui.cit260.lostteam.view.StartProgramView;
 
 /**
  *
@@ -28,72 +29,10 @@ public class LostTeam {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Player class test
-        Player playerOne = new Player();
-        
-        playerOne.setName("Fred Flintstone");
-        playerOne.setBestTime(7.00);
-        
-        String playerInfo = playerOne.toString();
-        System.out.println(playerInfo);
-        
-        
-        // Clue Class Test
-        Clue clueOne = new Clue();
-        
-        clueOne.setDescription("Level One");
-        clueOne.setReceived(true);
-        clueOne.setType(true);
-        
-        String clueInfo = clueOne.toString();
-        System.out.println(clueInfo);
-        
-        
-        // Location Class Test
-        Location locationOne = new Location();
-        
-        locationOne.setRow(1);
-        locationOne.setColumn(3);
-        locationOne.setVisited(true);
-        
-        String locationInfo = locationOne.toString();
-        System.out.println(locationInfo);
-        
-        
-        // Map Class Test
-        Map mapOne = new Map();
-        
-        mapOne.setRowCount(5);
-        mapOne.setColumnCount(5);
-        mapOne.setCurrentRow(2);
-        mapOne.setCurrentColumn(3);
-        mapOne.setCurrentScene("Level 8");
-        
-        String mapInfo = mapOne.toString();
-        System.out.println(mapInfo);
-         
-
-        //Item test by S. Bennett
-        Item itemOne = new Item();
-        
-        itemOne.setNoItems(5);
-        itemOne.setName("mushroom");
-        itemOne.setRequiredAmount(6);
-        
-        String itemInfo = itemOne.toString();
-        System.out.println(itemInfo);
-        
-        //Actor test by S. Bennett
-        Actor actorOne = new Actor();
-        
-        actorOne.setName("Scientist");
-        actorOne.setCooridinates("Subway");
-        actorOne.setSpokenTo(Boolean.TRUE);
-        actorOne.setClueGiven("See the Deli Owner");
-        
-        String actorInfo = actorOne.toString();
-        System.out.println(actorInfo);
-
+    
+    // create StartProgramViewOrig and display the start program view
+    StartProgramView startProgramView = new StartProgramView();
+    startProgramView.displayStartProgramView();
     }
     
 }
