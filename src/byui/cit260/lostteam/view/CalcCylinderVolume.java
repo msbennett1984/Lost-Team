3574abafd7dfba0c0bb82ef8a29aca9bea956715
Dallin Barlow <Dallin Barlow@LAPTOP-VKQ5VTM2>
2017-03-06 21:52:35 -0700

    Individual l09

---
+++ src/byui/cit260/lostteam/view/CalcCylinderVolume.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.lostteam.view;
+
+/**
+ *
+ * @author Dallin Barlow
+ */
+public class CalcCylinderVolume extends MenuView {
+    
+
+    public CalcCylinderVolume(){
+        super( "\n                                                 "
+        + "\n-------------------------------------------------------"
+        + "\nA police officer is investigating the scene of a crime "
+        + "\nthat was committed. 'It was a murder,' he tells you.   "
+        + "\nThe victim was shot with a 9 mm, we have a rough idea  "
+        + "\nof the distance, but we need to find the TRAJECTORY of "
+        + "\nthe bullet. Help us out and I will tell you something  "
+        + "\nuseful. Will you help?                                 "
+        + "\nY - Yes I will help                                    "
+        + "\nN - No I will not help (back to Scene Menu             ");
+
+        
+    }
+    @Override
+    public boolean doAction(String choice) {
+        // changes all values to uppercase
+        choice = choice.toUpperCase();
+        
+        switch (choice) {
+            case "S":
+                this.answerQuestion();
+                break;
+            case "B":
+                this.sceneMenu();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+        }
+        return false;
+    }
+
+    private void sceneMenu() {
+        SceneMenuView sceneMenu = new SceneMenuView();
+        sceneMenu.display();
+    }
+
+    private void answerQuestion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
--- src/byui/cit260/lostteam/view/SearchLocationView.java
+++ src/byui/cit260/lostteam/view/SearchLocationView.java
@@ -9,6 +9,44 @@ package byui.cit260.lostteam.view;
  *
  * @author Dallin Barlow
  */
-public class SearchLocationView {
+public class SearchLocationView extends MenuView {
     
+    SearchLocationView(){
+        super("\n"
+        + "\n----------------------------------------------"
+        + "\nWould you like to search your current location"
+        + "\n                 for Items?"
+        + "\nY - Yes (search for Items)"
+        +"\nN - No (No return to Scene Menu)"
+        + "\n");
+    }
+    
+    
+    @Override
+    public boolean doAction(String choice) {
+        // changes all values to uppercase
+        choice = choice.toUpperCase();
+        
+        switch (choice) {
+            case "Y":
+                this.searchLocation();
+                break;
+            case "N":
+                this.sceneMenu();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+        }
+        return false;
+    }
+
+    private void searchLocation() {
+        System.out.println("\n SearchLocation function found ");
+    }
+
+    private void sceneMenu() {
+        SceneMenuView sceneMenu = new SceneMenuView();
+        sceneMenu.display();
+    }
 }
