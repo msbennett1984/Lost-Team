4afbc2a6355075ba87c3efda17c553826af9e4e7
Sherry Bennett <msbennett84@gmail.com>
2017-03-06 19:41:52 -0700

    Week 9 Individual Assignment:
    * Commented out ErrorView.java (don't know why it was created yet)
    * Added CalcVolumeOfFlaskQuestionView

--- src/byui/cit260/lostteam/view/ErrorView.java
+++ src/byui/cit260/lostteam/view/ErrorView.java
@@ -3,32 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package byui.cit260.lostteam.view;
-
-import java.io.PrintWriter;
-import lostteam.LostTeam;
-
-/**
- *
- * @author adm-achina
- */
-public class ErrorView {
-    
-    private static final PrintWriter errorFile = LostTeam.getOutFile();
-    private static final PrintWriter logFile = LostTeam.getLogFile();
-    
-    /**
-     *
-     * @param className
-     * @param errorMessage
-     */
-    public static void display(String className, String errorMessage){
-        errorFile.println(
-                    "---------------------------------"
-                  + "\n- ERROR -" + errorMessage
-                  + "\n-------------------------------");
-        logFile.println(className + " - " +errorMessage);
-    
-}
-    
-}
+//package byui.cit260.lostteam.view;
+//
+//import java.io.PrintWriter;
+//import lostteam.LostTeam;
+//
+///**
+// *
+// * @author adm-achina
+// */
+//public class ErrorView {
+//    
+//    private static final PrintWriter errorFile = LostTeam.getOutFile();
+//    private static final PrintWriter logFile = LostTeam.getLogFile();
+//    
+//    /**
+//     *
+//     * @param className
+//     * @param errorMessage
+//     */
+//    public static void display(String className, String errorMessage){
+//        errorFile.println(
+//                    "---------------------------------"
+//                  + "\n- ERROR -" + errorMessage
+//                  + "\n-------------------------------");
+//        logFile.println(className + " - " +errorMessage);
+//    
+//}
+//    
+//}
